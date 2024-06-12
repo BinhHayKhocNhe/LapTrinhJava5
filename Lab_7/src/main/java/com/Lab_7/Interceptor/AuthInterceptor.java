1e62163c174040421cf8c802866babe7283a87d1
Admin <clonechuaxu1@gmail.com>
2024-06-12 08:53:00 +0700

    save bài

--- Lab_7/src/main/java/com/Lab_7/Interceptor/AuthInterceptor.java
+++ Lab_7/src/main/java/com/Lab_7/Interceptor/AuthInterceptor.java
@@ -15,24 +15,25 @@ public class AuthInterceptor implements HandlerInterceptor {
 	@Autowired
 	private SessionService session = null;
 
-	 @Override
-	    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-	        String uri = request.getRequestURI();
-	        Accounts user = session.getSession("user", ""); // lấy từ session
-	        String error = "";
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		String uri = request.getRequestURI();
+		Accounts user = session.getSession("user", ""); // lấy từ session
+		String error = "";
 
-	        if (user == null) { // chưa đăng nhập
-	            error = "Please login!";
-	        } else if (!user.isAdmin() && uri.startsWith("/login") && !uri.equals("/login")) { // không đúng vai trò
-	            error = "Access denied!";
-	        }
+		if (user == null) { // chưa đăng nhập
+			error = "Please login!";
+		} else if (!user.isAdmin() && uri.startsWith("/login") && !uri.equals("/login")) { // không đúng vai trò
+			error = "Access denied!";
+		}
 
-	        if (error.length() > 0) { // có lỗi
-	            session.setSession("security-uri", uri);
-	            response.sendRedirect("/login?error=" + error);
-	            return false;
-	        }
-	        return true;
-	    }
+		if (error.length() > 0) { // có lỗi
+			session.setSession("security-uri", uri);
+			response.sendRedirect("/login?error=" + error);
+			return false;
+		}
+		return true;
+	}
 
 }
